802646e25018ae39617a5cf862518735e882ed37
fabiosimplis <fabio.simplis@gmail.com>
2025-01-17 09:59:30 -0300

    add `lista suspensa de seleção` para selecionar os times

---
+++ src/components/ListaSuspensa/index.jsx
@@ -0,0 +1,16 @@
+import "./index.css";
+
+const ListaSuspensa = (props) => {
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+      <select>
+        {props.itens.map((item) => (
+          <option key={item}>{item}</option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
