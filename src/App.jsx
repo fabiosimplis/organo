d3dee29623cca9552c2b5ac5140045ee398c7a02
fabiosimplis <fabio.simplis@gmail.com>
2025-01-21 18:03:39 -0300

    add componente do time

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Banner from "./components/Banner/Banner";
 import Formulario from "./components/Formulario";
+import Time from "./components/Time";
 
 function App() {
 
@@ -15,6 +16,9 @@ function App() {
     <>
       <Banner />
       <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+      <Time nome="Programação"/>
+      <Time nome="Frontend"/>
+      <Time nome="Data Science"/>
     </>
   );
 }
