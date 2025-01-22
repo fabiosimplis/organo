dcab4b881a1969b88b644e0c965a9ead2e81122d
fabio.simplis <fabio.simplis@gmail.com>
2025-01-22 15:17:28 -0300

    add card colaborador

---
+++ src/components/Colaborador/index.jsx
@@ -0,0 +1,17 @@
+import './style.css'
+
+const Colaborador = () => {
+    return (
+        <div className='cabecalho'>
+            <div>
+                <img src="https://github.com/fabiosimplis.png" alt="Fabio Junior" />
+            </div>
+            <div className='rodape'>
+                <h4>Fábio Júnior</h4>
+                <h5>Desenvolvedor</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
