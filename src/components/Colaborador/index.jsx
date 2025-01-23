1c06bcf1c18afe0907fb5a5cdd5400aa79b71211
fabiosimplis <fabio.simplis@gmail.com>
2025-01-23 09:59:48 -0300

    add lib `react-icons` para inserir botão deletar

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,18 +1,19 @@
-import './style.css'
+import { AiFillCloseCircle } from "react-icons/ai";
+import "./style.css";
 
-const Colaborador = ({nome, cargo, imagem, corDeFundo, aoDeletar}) => {
-    return (
-        <div className='colaborador'>
-            <div className='deletar' onClick={aoDeletar}>deletar</div>
-            <div className='cabecalho' style={{backgroundColor: corDeFundo}}>
-                <img src={imagem} alt={nome} />
-            </div>
-            <div className='rodape'>
-                <h4>{nome}</h4>
-                <h5>{cargo}</h5>
-            </div>
-        </div>
-    )
-}
+const Colaborador = ({ nome, cargo, imagem, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={imagem} alt={nome} />
+      </div>
+      <div className="rodape">
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
+      </div>
+    </div>
+  );
+};
 
-export default Colaborador
+export default Colaborador;
