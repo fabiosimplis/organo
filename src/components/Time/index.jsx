1b2fea92d83d203ac78c8c59460a9cb61bc77130
fabiosimplis <fabio.simplis@gmail.com>
2025-01-23 17:14:29 -0300

    add ids

--- src/components/Time/index.jsx
+++ src/components/Time/index.jsx
@@ -8,13 +8,16 @@ const Time = ({ time, colaboradores, mudarCor, aoDeletar }) => {
     colaboradores.length > 0 && (
       <section
         className="time"
-        style={{ backgroundImage: "url(/imagens/fundo.png)", backgroundColor: hexToRgba(time.cor, '0.6')}}
+        style={{
+          backgroundImage: "url(/imagens/fundo.png)",
+          backgroundColor: hexToRgba(time.cor, "0.6"),
+        }}
       >
         <input
           value={time.cor}
           type="color"
           className="input-cor"
-          onChange={(evento) => mudarCor(evento.target.value, time.nome)}
+          onChange={(evento) => mudarCor(evento.target.value, time.id)}
         />
         <h3 style={{ borderColor: time.cor }}>{time.nome}</h3>
         <div className="colaboradores">
