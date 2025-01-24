b9865a43642e5ee0cbe4538fd721037c1490030d
fabiosimplis <fabio.simplis@gmail.com>
2025-01-24 10:14:16 -0300

    add refatoração do formulário

--- src/components/Formulario/index.jsx
+++ src/components/Formulario/index.jsx
@@ -1,5 +1,5 @@
 import "./style.css";
-import CampoTexto from "../CampoTexto";
+import Campo from "../Campo";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
 import { useState } from "react";
@@ -28,24 +28,24 @@ const Formulario = (props) => {
   };
 
   return (
-    <section className="formulario">
-      <form onSubmit={aoSalvar}>
+    <section className="formulario-container">
+      <form className="formulario" onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Nome"
           placeholder="Digite o seu nome"
           valor={nome}
           aoAlterar={(valor) => setNome(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Cargo"
           placeholder="Digite o seu cargo"
           valor={cargo}
           aoAlterar={(valor) => setCargo(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio={true}
           label="Imagem"
           placeholder="Digite o endereço da imagem"
@@ -62,21 +62,25 @@ const Formulario = (props) => {
         <Botao>Criar Card</Botao>
       </form>
       <form
+        className="formulario"
         onSubmit={(evento) => {
           evento.preventDefault();
           props.cadastrarTime({ nome: nomeTime, cor: corTime });
+          setNomeTime("");
+          setCorTime("");
         }}
       >
         <h2>Preencha os dados para criar um novo time.</h2>
-        <CampoTexto
+        <Campo
           obrigatorio
           label="Nome"
           placeholder="Digite o nome do time"
           valor={nomeTime}
           aoAlterar={(valor) => setNomeTime(valor)}
         />
-        <CampoTexto
+        <Campo
           obrigatorio
+          type='color'
           label="Cor"
           placeholder="Digite a cor do time"
           valor={corTime}
